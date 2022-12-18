ebc8dfac6ca4e727140c494c0e1dd67ef32085ef
vargasmichael <119553981+vargasmichael@users.noreply.github.com>
2022-12-18 09:04:05 -0700

    commit tree c068fafa0a002e156d2e95cd5982551aac60ce32

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+function writeCards(namesArray, event) {
+    let array = []
+for (let i = 0; i < namesArray.length; i++){
+    array.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+}
+return array;
+
+}
+
+function countDown() {
+    let i = 0;
+
+    while(i < 11){
+        console.log(i);
+    i++
+}
+}
